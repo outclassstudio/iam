b727bb1352bd9d720cb27fd9bf19b4aaef7e0c5b
outclassstudio <mlimshs@gmail.com>
2023-04-28 22:22:13 +0900

    Feat: Add favicon, about page

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import About from "./pages/about/About";
 import Main from "./pages/main/Main";
 import { GlobalStyle } from "./style/global.style";
 
@@ -8,6 +9,7 @@ function App() {
       <GlobalStyle />
       <Routes>
         <Route path="/" element={<Main />} />
+        <Route path="/about" element={<About />} />
       </Routes>
     </Router>
   );
---
+++ src/pages/about/About.tsx
@@ -0,0 +1,9 @@
+import Layout from "../Layout";
+
+export default function About() {
+  return (
+    <Layout title="about">
+      <></>
+    </Layout>
+  );
+}
