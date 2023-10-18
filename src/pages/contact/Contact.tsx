526f1f36742f0719109b3ae1089d03102dc6e6c4
Higashino-Keigo <kooa3399@kakao.com>
2023-10-18 10:59:01 +0900

    change Footer2

--- src/pages/contact/Contact.tsx
+++ src/pages/contact/Contact.tsx
@@ -1,17 +1,9 @@
-import { useState, useRef } from "react";
 import styled from "styled-components";
 import AnotherHeader from "../../components/AnotherHeader";
 import { fadeAction, mediaQuery } from "../../style/global.style";
 import Layout from "../Layout";
 
 export default function Contact() {
-  const [currentPage, setCurrentPage] = useState("");
-
-  const handleCurrentPage = (page: string) => setCurrentPage(page);
-
-  const content1Ref = useRef<HTMLDivElement>(null);
-  const content2Ref = useRef<HTMLDivElement>(null);
-
   return (
     <Layout>
       <AnotherHeader />
@@ -26,45 +18,8 @@ export default function Contact() {
             <SubText>12:00 - 13:00</SubText>
             <SubTitle>제휴/문의</SubTitle>
             <SubText>iamcreatorss@gmail.com</SubText>
-            <Spacer/>
-            <SubHyperLink
-              onClick={() => {
-                handleCurrentPage("Terms1");
-                setTimeout(() => {
-                  content1Ref.current?.scrollIntoView({behavior: 'smooth'});
-                }, 100);
-              }
-            }
-              >
-              이메일 무단수집 거부
-            </SubHyperLink>
-            <SubHyperLink
-              onClick={() => {
-                handleCurrentPage("Terms2");
-                setTimeout(() => {
-                  content2Ref.current?.scrollIntoView({behavior: 'smooth'});
-                }, 100);
-              }
-            }
-              >
-              개인정보 수집 이용 약관
-            </SubHyperLink>
           </ContactContentWrapper>
         </ContactWrapper>
-        {currentPage === "Terms1" ? (
-          <>
-          <Spacer ref={content1Ref}/>
-          <TermsTitle>이메일 무단수집 거부</TermsTitle>
-          <EmailTerms></EmailTerms>
-          </>
-        ) : null}
-        {currentPage === "Terms2" ? (
-          <>
-          <Spacer ref={content2Ref}/>
-          <TermsTitle>개인정보 수집 이용 약관</TermsTitle>
-          <PersonalPolicy></PersonalPolicy>   
-          </>
-        ) : null}
       </ContactPageContainer>
     </Layout>
   );
@@ -110,9 +65,8 @@ const ContactImage = styled.img`
 `;
 const ContactContentWrapper = styled.div`
   display: flex;
-  align-content: center;
   flex-direction: column;
-  justify-content: flex-end;
+  justify-content: center;
   margin-left: 25px;
   /* align-items: center; */
 `;
@@ -131,53 +85,4 @@ const SubText = styled.div`
   ${mediaQuery.mobile} {
     font-size: 16px;
   }
-`;
-const SubHyperLink = styled.div`
-  font-size: 18px;
-  color: #636363;
-  text-decoration: underline;
-  margin-top: 6px;
-  cursor: pointer;
-`;
-const Spacer = styled.div`
-  margin-top:95px;
-`;
-const TermsTitle = styled.div`
-  text-align: center;
-  font-size: 32px;
-  font-weight: 600;
-  line-height: 143.992%; /* 46.077px */
-
-  ${mediaQuery.mobile} {
-    font-size: 18px;
-    margin-bottom: 10px;
-  }
-`;
-const EmailTerms = styled.div`
-  background-image: url("/assets/email_terms.png");
-  background-size: contain;
-  background-position: center;
-  background-repeat: no-repeat;
-  width: 669px;
-  height: 343px;
-  margin-bottom: 60px;
-
-  ${mediaQuery.mobile} {
-    width: 335px;
-    height: 172px;
-  }
-`;
-const PersonalPolicy = styled.div`
-background-image: url("/assets/personal_policy.png");
-background-size: contain;
-background-position: center;
-width: 669px;
-height: 3700px;
-background-repeat: no-repeat;
-margin-bottom: 60px;
-
-${mediaQuery.mobile} {
-  width: 335px;
-  height: 1720px;
-}
 `;
